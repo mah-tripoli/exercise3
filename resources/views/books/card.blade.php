a3d42639a8cc846dc10f23593d4c023bc2936de0
Wesam Alalem <wesam@email.ly>
2024-02-01 15:55:57 +0200

    update book card view

--- resources/views/books/card.blade.php
+++ resources/views/books/card.blade.php
@@ -1,13 +1,13 @@
 <div class="card">
     <img src="https://dummyimage.com/150x125/cccccc/ffffff.png&text=Book+Cover" alt="{{ $book['title'] }}" class="card-img-top">
     <div class="card-body">
-        <strong>{{ $book['title'] }}</strong>
-        <p>{{ $book['author'] }}</p>
-        <p>{{ $book['publication_year'] }}</p>
-
-        @if ($book['available'])
+        <strong>{{ $book->title }}</strong>
+        <p>{{ $book->author }}</p>
+        <p>{{ $book->publish_year }}</p>
+        <p><span class="badge text-bg-info">{{ $book->genre?->name }}</span></p>
+        @if ($book->available_quantity > 0 && !$book->userRented)
             <button class="btn btn-primary" onclick="rentBook({{ $book['id'] }})">Rent Book</button>
-        @else
+        @elseif ($book->userRented)
             <button class="btn btn-warning" onclick="returnBook({{ $book['id'] }})">Return Book</button>
         @endif
         
